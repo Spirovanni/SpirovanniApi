c8989e7c85a2bd4d655c8efd99a7cbaab95cee41
Xavier Martinez <blackshieldsx@gmail.com>
2020-12-14 18:42:14 -0800

    Finding and printing user in views

--- api/views.py
+++ api/views.py
@@ -2,6 +2,7 @@ from rest_framework import viewsets, status
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from django.shortcuts import render
+from django.contrib.auth.models import User
 from .models import Card, Rating
 from .serializers import CardSerializer, RatingSerializer, UserSerializer
 
@@ -12,8 +13,14 @@ class CardViewSet(viewsets.ModelViewSet):
 
     @action(detail=True, methods=['POST'])
     def rate_card(self, request, pk=None):
-
         if 'stars' in request.data:
+
+            card = Card.objects.get(id=pk)
+            stars = request.data['stars']
+            # user = request.user
+            user = User.objects.get(id=1)
+            print('user', user.username)
+
             response = {'message': 'Its working'}
             return Response(response, status=status.HTTP_202_ACCEPTED)
         else:
